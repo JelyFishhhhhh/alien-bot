281a4420950fcb22d782d29be27b8e0195dfde00
JelyFishhhhhh_ _ <dlanfongtwb@gmail.com>
2022-04-02 22:47:19 +0800

    on ready msg

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from email import message
 import discord
 from discord.ext import commands
 import datetime
@@ -5,7 +6,6 @@ import sys
 import os
 from os import mkdir, path
 import json
-from json.decoder import JSONDecodeError
 
 prefix='*'
 client = commands.Bot(command_prefix=prefix,intents=discord.Intents.all())
@@ -22,6 +22,8 @@ for x in cogs:
 @client.event
 async def on_ready():
     print(f'{datetime.datetime.now():%Y-%m-%d %H:%M:%S} >>> {client.user} is on the air.')
+    #channel = client.get_channel(int(data['statusChannel']))
+    #await channel.send(f'<@{client.user.id}> is on the air !')
 
 if __name__ == '__main__':
     client.run(data['TOKEN'])
